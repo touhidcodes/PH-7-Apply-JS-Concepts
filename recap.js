b37cf0061ce3f5acdd84aa35dd0979986ca40a8b
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-31 01:18:50 +0600

    recap.js

---
+++ recap.js
@@ -0,0 +1,33 @@
+/* 
+1. variable
+ math operations: +, -, *, /
+ shorthand: +=, -=, *=, /=
+
+*/
+// 2. Array
+var friends = ["hablu", "Mablu", "gablu"];
+
+friends[3] = "pagol";
+
+// 3. Conditionals
+// <, >, <=, >=, ==, ===
+
+// 4. loop
+for (let i = 0; i < 6; i++) {
+	console.log("hi");
+}
+
+// 5. Function
+function isMoonUp(time) {
+	if (time > 7) {
+		return true;
+	}
+}
+
+// 6.Object
+const students = {
+	name: "abul",
+	id: 24,
+	salary: 24000,
+	education: "fail",
+};
