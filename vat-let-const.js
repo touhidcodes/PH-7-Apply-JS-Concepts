6f482481d6fd0010f7c93e6a7efcc58498d4e131
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-31 01:25:46 +0600

    let-var-const

---
+++ vat-let-const.js
@@ -0,0 +1,10 @@
+// var have no rules in declaration and it can re declare
+var money = 23;
+var money = 45;
+var money = 67;
+
+// let can't re declare variable
+let product = 3;
+
+// const value always fixed
+const value = 23;
