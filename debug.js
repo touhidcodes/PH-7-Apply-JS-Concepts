fe77a1c700018cea8396febce2c125138ddd5471
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-31 19:55:36 +0600

    debug

---
+++ debug.js
@@ -0,0 +1,10 @@
+function factorial(number) {
+	const result = 0;
+	for (let i = 0; i < number; i++) {
+		result *= i;
+	}
+}
+
+// debug with two methods
+// 1. debug code
+// 2. Terminal error (console)
--- factorialReverse.js
+++ factorialReverse.js
@@ -1,6 +1,6 @@
 function factorial(number) {
 	let result = 1;
-	for (let i = number; i > 0; i--) {
+	for (let i = number; i >= 1; i--) {
 		result *= i;
 	}
 	return result;
