78a6e3501ecff7b0fa65b9356feec16b2ab94db4
Touhidur Zaman <touhidcodes@gmail.com>
2023-01-31 19:12:41 +0600

    odd-sum

--- findOddSum.js
+++ findOddSum.js
@@ -1,4 +1,20 @@
-const arrayNumbers = [12, 34, 45, 56, 67, 78, 89];
+const arrayNumbers = [12, 34, 45, 56, 67, 78, 89, 12, 45, 34, 33, 99, 66, 67];
+
+function getOddNumbers(numbers) {
+	const odd = [];
+	for (let i = 0; i < numbers.length; i++) {
+		const element = numbers[i];
+		// console.log(i, element);
+		if (element % 2 !== 0) {
+			// console.log("odd");
+			odd.push(element);
+		}
+	}
+	return odd;
+}
+
+const oddNumbers = getOddNumbers(arrayNumbers);
+console.log(oddNumbers);
 
 function getSumOfArray(numbers) {
 	let sum = 0;
@@ -11,5 +27,5 @@ function getSumOfArray(numbers) {
 	return sum;
 }
 
-const output = getSumOfArray(arrayNumbers);
+const output = getSumOfArray(oddNumbers);
 console.log(output);
